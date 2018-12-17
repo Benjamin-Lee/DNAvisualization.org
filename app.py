713fa22b9fc10b60e8a33d0561b924410ffe6dbd
Benjamin Lee <benjamindlee@me.com>
2018-12-17 02:10:09 -0500

    Store and query using s3

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ import xxhash
 import os.path
 import logging
 
+from aws import exists_on_s3, upload, query_x_range
+
 logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('botocore').setLevel(logging.INFO)
+logging.getLogger('urllib3').setLevel(logging.INFO)
+logging.getLogger('s3transfer').setLevel(logging.INFO)
+
+
+LOCAL = False
+logging.info("Running locally" if LOCAL else "Running on AWS")
 
 app = Flask(__name__)
 
@@ -18,8 +27,16 @@ def index():
 @app.route("/seq_query")
 def seq_query():
     # takes a seq hash and returns a downsampled region
-    logging.debug("Getting data for seq ID {}".format(request.args["seq_hash"]))
-    df = pd.read_parquet("data/" + str(request.args["seq_hash"]) + ".parquet.sz")
+    logging.debug(f"Getting data for seq ID {request.args['seq_hash']}")
+
+    if LOCAL:
+        df = pd.read_parquet("data/" + str(request.args["seq_hash"]) + ".parquet.sz")
+    else:
+        df = query_x_range(str(request.args["seq_hash"]) + ".parquet.sz",
+                           request.args.get("x_min"),
+                           request.args.get("x_max"))
+
+    logging.debug("Got the data")
 
     zone = df.loc[(float(request.args.get("x_max", df.x.max())) >= df.x) &
                   (float(request.args.get("x_min", df.x.min())) <= df.x)].values
@@ -28,7 +45,9 @@ def seq_query():
     # downsample if > 5000 points
     # TODO: ensure that the first and last data points in the range are included
     if len(zone) > 5000:
-        zone = zone[::int(len(zone) / 5000)]
+        downsample = int(len(zone) / 5000)
+        logging.debug(f"Downsampling by a factor of {downsample}")
+        zone = zone[::downsample]
 
     return jsonify({"name": request.args["seq_id"],
                     "data": zone,
@@ -42,20 +61,31 @@ def parse_fasta():
 
     for seq in read([x.decode("ascii") for x in request.files["sequence"].readlines()], "fasta"):
 
-        logging.debug("hashing")
+        logging.debug("Hashing seq")
         seq_hash = str(xxhash.xxh64(str(seq)).intdigest())
 
-        if not os.path.exists("data/" + seq_hash + ".parquet.sz"):
-            logging.debug("No existing transformed version of {}. Transforming...".format(seq.metadata["id"]))
+        if LOCAL:
+            exists = os.path.exists("data/" + seq_hash + ".parquet.sz")
+            logging.debug(f"Found {seq_hash} locally")
+        else:
+            exists = exists_on_s3(seq_hash + ".parquet.sz")
+            logging.debug(f"Found {seq_hash} on S3")
+
+        if not exists:
+            logging.debug("No previous transformation found. Transforming...")
             transformed = transform(str(seq))
 
             logging.debug("Saving transformed data for " + seq.metadata["id"])
             pd.DataFrame(dict(x=transformed[0], y=transformed[1])).to_parquet("data/" + seq_hash + ".parquet.sz")
-        else:
-            logging.debug("Existing transformation found.")
+
+            if not LOCAL:
+                logging.debug(f"Uploading {seq_hash} to S3")
+                upload(seq_hash + ".parquet.sz")
+
         results.append(dict(seq_hash=seq_hash,
                             seq_id=seq.metadata["id"],
                             seq_filename=request.files["sequence"].filename))
+
     return jsonify(results)
 
 
---
+++ aws.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+import boto3
+import io
+import pandas as pd
+import tempfile
+
+s3 = boto3.client('s3')
+
+def exists_on_s3(key, client=s3, bucket="squiggle-data"):
+    """return the key's size if it exist, else None"""
+    response = client.list_objects_v2(
+        Bucket=bucket,
+        Prefix=key,
+    )
+    for obj in response.get('Contents', []):
+        if obj['Key'] == key:
+            return obj['Size']
+
+
+def upload(filename):
+    return s3.upload_file("data/" + filename,
+                          "squiggle-data",
+                          filename)
+
+def query_x_range(Key, x_min=None, x_max=None):
+
+    if x_min and x_max:
+        Expression = "select * from s3object s where x > {} AND x < {};".format(x_min, x_max)
+    else:
+        Expression = "select * from s3object s;"
+
+    response = s3.select_object_content(Bucket="squiggle-data",
+                                        Key=Key,
+                                        Expression=Expression,
+                                        ExpressionType="SQL",
+                                        InputSerialization={'Parquet': {}},
+                                        OutputSerialization={'CSV': {'RecordDelimiter': '\n'}})
+    event_stream = response['Payload']
+    end_event_received = False
+    # data_bytes = io.BytesIO()
+    fp = tempfile.TemporaryFile()
+    for event in event_stream:
+        if 'Records' in event:
+            data = event['Records']['Payload']
+            # with open(fp, "ab+") as f:
+            fp.write(data)
+
+        # If we received a progress event, print the details
+        elif 'Progress' in event:
+            print(event['Progress']['Details'])
+
+        # End event indicates that the request finished successfully
+        elif 'End' in event:
+            end_event_received = True
+            # print(data_bytes.getvalue())
+            fp.seek(0)
+            df = pd.read_csv(fp, names=["x", "y", "index"]).drop(columns=["index"])
+            fp.close()
+            return df
+
+    if not end_event_received:
+        fp.close()
+        raise Exception("End event not received, request incomplete.")
