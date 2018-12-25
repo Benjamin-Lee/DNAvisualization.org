09d1cd57b21bde9840fc7cd5f28d9ffe8643ecf8
Mick Timony <mick.timony+github.com@gmail.com>
2018-12-25 14:51:39 -0500

    jumbotron disappears when the plot is called

--- static/js/home.js
+++ static/js/home.js
@@ -123,7 +123,7 @@ window.onload = function () {
               })
           })
 
-        document.querySelector(".hide-when-plotting").style.display = "none"; // get rid of the jumbotron
+        document.getElementById("hide-when-plotting").style.display = "none"; // get rid of the jumbotron
         document.getElementById("hg-container").style.display = "block"; // after dropping, show chart div
       }
     }
