08c6f1d753bb26179ee1f38f31927d0a700d45ae
Benjamin Lee <benjamindlee@me.com>
2020-07-01 16:47:41 -0700

    Autodiscover logo component on index page

--- pages/index.vue
+++ pages/index.vue
@@ -24,16 +24,6 @@
   </div>
 </template>
 
-<script>
-import Logo from "~/components/Logo.vue"
-
-export default {
-  components: {
-    Logo,
-  },
-}
-</script>
-
 <style>
 .container {
   margin: 0 auto;
