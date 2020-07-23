1d459de91063a6d78b82a3e3181e97e9345cec9f
Meha Goyal <mehagoyal123@gmail.com>
2020-07-23 15:06:01 -0700

    Add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
-    <b-navbar></b-navbar>
-    <nuxt />
+    <b-navbar
+      toggleable="lg"
+      type="light"
+      variant="info"
+      class="navbar navbar-expand-lg navbar-light bg-light rounded sticky-top"
+    >
+      <b-navbar-brand href="#">DNAvisualization.org</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#">Instructions</b-nav-item>
+          <b-nav-item href="#">Architecture</b-nav-item>
+          <b-nav-item href="#">Citation</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+          <b-nav-item
+            href="https://github.com/Benjamin-Lee/DNAvisualization.org/issues/new?assignees=&amp;labels=bug&amp;template=bug_report.md&amp;title="
+            target="_blank"
+            >Report a Bug</b-nav-item
+          >
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <nuxt></nuxt>
   </div>
 </template>
