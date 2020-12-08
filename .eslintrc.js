0b7e1ae928c85ba1c6e237fb40a40ecd7c0cec66
Caleb Oh <caleboh01@gmail.com>
2020-12-07 22:03:40 -0500

    reconfigured prettier eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,9 @@ module.exports = {
   ],
   plugins: ["prettier"],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "prettier/prettier": ["error",{
+      "endOfLine": "auto"}
+    ]
+  },
 }
