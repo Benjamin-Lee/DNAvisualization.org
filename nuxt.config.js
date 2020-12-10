001379b30f63c9303f0ac39da4891440c3447b84
Benjamin Lee <benjamindlee@me.com>
2020-12-10 02:53:26 -0500

    Close #89

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
         content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "icons/favicon.ico" }],
   },
   /*
    ** Customize the progress-bar color
